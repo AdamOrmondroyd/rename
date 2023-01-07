06b95af8f4ba17c6e62345114efa2b19cfe3f8ff
Ormorod <Adam.Ormondroyd@gmail.com>
2023-01-07 20:41:25 +0000

    remove underscores

--- scripts/cmd.py
+++ scripts/cmd.py
@@ -1,6 +1,6 @@
 import os
-from rename_images.rename_photos import rename_photos
-from rename_images.rename_screenshots import rename_screenshots
+from rename.rename_photos import rename_photos
+from rename.rename_screenshots import rename_screenshots
 
 
 def cmd_rename_photos():
